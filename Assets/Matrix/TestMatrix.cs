f004ee19201ab4f92e7225f1f80ed12cd5bda00b
Xacoris <braydensoh04@gmail.com>
2023-10-30 17:16:00 +0800

    Matrix try

--- Assets/Matrix/TestMatrix.cs
+++ Assets/Matrix/TestMatrix.cs
@@ -21,6 +21,15 @@ public class TestMatrix : MonoBehaviour
 
     void Question2()
     {
+        HMatrix2D mat1 = new HMatrix2D(1,2,3,4,5,6,7,8,9);
+        HMatrix2D mat2 = new HMatrix2D(-2,1,7,4,-2,-4,5,-7,8);
+        HMatrix2D resultMat = new HMatrix2D();
+        HVector2D vec1 = new HVector2D(3,4);
+        HVector2D resultVec = new HVector2D();
 
+        resultMat = mat1 * mat2;
+        print(resultMat);
+        resultVec = mat2 * vec1;
+        Debug.Log(resultVec);
     }
 }
