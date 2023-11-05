a1e4455ba7d23537a66160f84546aa3953b0d423
Xacoris <braydensoh04@gmail.com>
2023-11-05 18:07:37 +0800

    Q5 complete? I matrix cmi

---
+++ Assets/02_MATRICES_worksheet/02_MATRICES_worksheet/Sonic/TransformMesh.cs
@@ -0,0 +1,74 @@
+﻿//using Mono.Cecil.Cil;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformMesh : MonoBehaviour
+{
+    [HideInInspector]
+    public Vector3[] vertices { get; private set; }
+
+    private HMatrix2D transformMatrix = new HMatrix2D();
+    HMatrix2D toOriginMatrix = new HMatrix2D();
+    HMatrix2D fromOriginMatrix = new HMatrix2D();
+    HMatrix2D rotateMatrix = new HMatrix2D();
+
+    private MeshManager meshManager;
+    HVector2D pos = new HVector2D();
+
+    void Start()
+    {
+        meshManager = GetComponent<MeshManager>();
+        pos = new HVector2D(gameObject.transform.position.x, gameObject.transform.position.y);
+
+        // Your code here
+        Translate(1, 1);
+        Rotate(45);
+    }
+
+
+    void Translate(float x, float y)
+    {
+        transformMatrix.setIdentity();
+        transformMatrix.setTranslationMat(x,y);
+        Transform();
+
+        pos = transformMatrix * pos;
+    }
+
+    void Rotate(float angle)
+    {
+        HMatrix2D toOriginMatrix = new HMatrix2D();
+        HMatrix2D rotateMatrix = new HMatrix2D();
+        HMatrix2D fromOriginMatrix = new HMatrix2D();
+
+        toOriginMatrix.setTranslationMat(-transform.position.x,-transform.position.y);
+        fromOriginMatrix.setTranslationMat(transform.position.x, transform.position.y);
+        rotateMatrix.setRotationMat(angle);
+
+        transformMatrix.setIdentity();
+
+        // Your code here
+
+        transformMatrix = fromOriginMatrix * rotateMatrix * toOriginMatrix;
+        // Your code here;
+
+        Transform();
+    }
+
+    private void Transform()
+    {
+        vertices = meshManager.clonedMesh.vertices;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            // Your code here
+            HVector2D vert = new HVector2D(vertices[i].x, vertices[i].y);
+            vert = transformMatrix * vert;
+            vertices[i].x = vert.x;
+            vertices[i].y = vert.y;
+        }
+
+        meshManager.clonedMesh.vertices = vertices;
+    }
+}
